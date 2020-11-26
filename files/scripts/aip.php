2916766d14ee4ac3ec5e1761b8f4aff7b92f06b5
Nicolas Frugier <nicolas.frugier2@gmail.com>
2020-11-26 02:53:50 +0100

    add emailing results to GM

--- files/scripts/aip.php
+++ files/scripts/aip.php
@@ -24,6 +24,11 @@
     if (empty($name)) {
         $name = '...';
     }
+
+    $to = 'nicolas.frugier2+test@gmail.com';
+    $subject = 'AIP de '.$name;
+    $headers = 'MIME-Version: 1.0'. "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1'.'\r\n';
 ?>
 <html>
 <head>
@@ -72,20 +77,49 @@
                 $style = "";
                 break;
         }
-
-        echo '<tr class="'.$style.'">
+        $message[] = '<tr class="'.$style.'">
                 <td>' . $action . '</td>
                 <td>' . ucfirst($givenSkills[$key]) . '</td>
                 <td>' . $d20s[$key] . $operator . $skillBonus . ' = <b>' . $result[$key] . '</b></td>
               </tr>';
 
+        echo $message[$key];
+
     }
 
+    //EMAIL
+
+    $body = '
+    <html>
+    <head>
+        <title>'.$subject.'</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    </head>
+    <body>
+        <table class="table table-hover table-responsive">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Action</th>
+                    <th>Compétence</th>
+                    <th>Résultat</th>
+                </tr>
+            </thead>
+            <tbody>'
+                .$message[0].$message[1].$message[2].$message[3].$message[4].$message[5].$message[6].
+            '</tbody>
+        </table>
+    </body>
+    </html>
+    ';
+
+    $resultMail = mail($to, $subject, $body, $headers);
     ?>
     </tbody>
 </table>
 </div>
 <footer>Jets de dés effectués avec la méthode PHP <a href="https://www.php.net/manual/fr/function.random-int.php">random_int()</a>.
-    </br>Code par <a href="https://github.com/nfrugier/AIPmanager">Thrandal</a></footer>
+    </br>Code par <a href="https://github.com/nfrugier/AIPmanager">Thrandal</a>
+    </br><?php if($resultMail) {echo 'Un email avec le résultat a été envoyé à votre MJ.';} else {echo 'Une erreur s\'est produite durant l\'envoie de l\'email au MJ';}  ?></footer>
 </body>
 </html>
